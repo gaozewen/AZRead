11b01307d96bf21c0cd10dbc1c11d5fba9244b95
GaoZeWen <1440651163@qq.com>
2024-03-09 23:31:50 +0800

    feat: book-list 组件编写

---
+++ components/book-list.vue
@@ -0,0 +1,30 @@
+<template>
+	<view :style="wrapperStyle">
+		<block v-for="book in bookListArr" :key="book.id">
+			<view class="flex align-center p-1 border-bottom" hover-class="bg-light" style="height: 320rpx;">
+				<image :src="book.imgurl" mode="aspectFill" class="rounded mx-2" style="width: 250rpx;height: 250rpx;">
+				</image>
+				<view class="flex-1 pt-2" style="height: 100%;">
+					<view class="py-2 font-md font-weight-bold"> {{book.name}} </view>
+					<view class="font text-light-black"> {{book.synopsis}} </view>
+				</view>
+			</view>
+		</block>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "book-list",
+		props: {
+			bookListArr: {
+				type: Array,
+				default: []
+			},
+			wrapperStyle: {
+				type: String,
+				default: ''
+			}
+		}
+	}
+</script>
