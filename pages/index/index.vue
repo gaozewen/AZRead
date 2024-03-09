4b512c058f655d3b8ba78a38667d4ca7ff2633de
GaoZeWen <1440651163@qq.com>
2024-03-09 10:27:21 +0800

    init project

---
+++ pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+	<view>
+		<view class="status_bar">
+			<!-- 这里是状态栏 -->
+		</view>
+		<!-- <view :style="{height:`${statusBarHeight}px`,background:'pink'}"></view> -->
+		<view>{{statusBarHeight}}</view>
+	</view>
+</template>
+
+<script>
+	const statusBarHeight = uni.getSystemInfoSync().statusBarHeight
+
+	export default {
+		data() {
+			return {
+				statusBarHeight
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {}
+	}
+</script>
+
+<style>
+	.status_bar {
+		height: var(--status-bar-height);
+		width: 100%;
+		background-color: royalblue;
+	}
+</style>
